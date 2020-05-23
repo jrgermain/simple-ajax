9c108bff2c9b8cf4cfb5d0902d1f214cd214763c
jrgermain <37086292+jrgermain@users.noreply.github.com>
2020-05-23 15:43:35 -0400

    Use destructuring to clean up code

--- ajax.js
+++ ajax.js
@@ -2,38 +2,39 @@
 "use strict";
 
 const Ajax = {
-    request: function (params) {
-        // Check for invalid or missing parameters
-        const paramType = (params == null) ? params : params.constructor.name;
-
-        if (paramType !== "Object") throw "Ajax: request parameter must be a plain object, not " + paramType;
-        if (params.method == null || params.url == null) throw "Ajax: missing required parameter(s)";
+    request: function ({ method, url, requestHeader, requestBody, onSuccess, onError }) {
+        if (method == null || url == null) {
+            throw "Ajax: missing required parameter(s)";
+        }
 
         return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
-            xhr.open(params.method, params.url);
+            xhr.open(method, url);
 
             // Set request header if present
-            if (Array.isArray(params.requestHeader)) {
-                for (const header of params.requestHeader)
+            if (Array.isArray(requestHeader)) {
+                for (const header of requestHeader) {
                     xhr.setRequestHeader(header.name, header.value);
-            } else if (typeof params.requestHeader === "object") {
-                xhr.setRequestHeader(params.requestHeader.name, params.requestHeader.value);
-            } else if (params.requestHeader) {
+                }
+            } else if (typeof requestHeader === "object") {
+                xhr.setRequestHeader(requestHeader.name, requestHeader.value);
+            } else if (requestHeader) {
                 throw "Ajax: Invalid request header. Must be a single object or array of objects";
             }
 
-            xhr.send(params.requestBody);
+            xhr.send(requestBody);
             xhr.onreadystatechange = () => {
                 if (xhr.readyState === 4) {
                     if (xhr.status >= 200 && xhr.status < 300) {
                         resolve(xhr);
-                        if (typeof params.onSuccess === "function")
-                            params.onSuccess(xhr);
+                        if (typeof onSuccess === "function") {
+                            onSuccess(xhr);
+                        }
                     } else {
                         reject(xhr);
-                        if (typeof params.onError === "function")
-                            params.onError(xhr);
+                        if (typeof onError === "function") {
+                            onError(xhr);
+                        }
                     }
                 }
             };
